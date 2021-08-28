5d490d55be76f393f10844dbdd46d616cee68464
Matheus Woeffel <matheuswoeffel@gmail.com>
2021-08-28 10:41:58 -0300

    Implemented a* using manhattan heuristic

--- solucao.py
+++ solucao.py
@@ -1,7 +1,7 @@
 import math
 from src.nodo import Nodo
 from src.fronteiras import Fronteira, FilaFronteira, HeapFronteira, PilhaFronteira
-from src.heuristicas import distancia_de_hamming
+from src.heuristicas import distancia_de_hamming, distancia_de_manhattan
 import unittest
 
 
@@ -71,21 +71,27 @@ def dfs(estado=''):
 def astar_hamming(estado=''):
   return buscar(estado, HeapFronteira(distancia_de_hamming))
 
+def astar_manhattan(estado=''):
+  return buscar(estado, HeapFronteira(distancia_de_manhattan))
+
 
 class GraphSearchMethods(unittest.TestCase):
   def test_simple(self):
     self.assertEqual(['direita'], dfs('1234567_8'))
     self.assertEqual(['direita'], bfs('1234567_8'))
     self.assertEqual(['direita'], astar_hamming('1234567_8'))
+    self.assertEqual(['direita'], astar_manhattan('1234567_8'))
 
   def test_no_solution(self):
     self.assertEqual(None, bfs('185423_67'))
     self.assertEqual(None, dfs('185423_67'))
     self.assertEqual(None, astar_hamming('185423_67'))
+    self.assertEqual(None, astar_manhattan('185423_67'))
 
   def test_intermediate_solution_bfs(self):
     self.assertEqual(['abaixo', 'abaixo', 'direita', 'direita'], bfs('_23156478'))
     self.assertEqual(['abaixo', 'abaixo', 'direita', 'direita'], astar_hamming('_23156478'))
+    self.assertEqual(['abaixo', 'abaixo', 'direita', 'direita'], astar_manhattan('_23156478'))
 
   def test_intermediate_solution_dfs(self):
     self.assertEqual(['esquerda', 'esquerda', 'abaixo', 'direita', 'direita', 
@@ -95,6 +101,7 @@ class GraphSearchMethods(unittest.TestCase):
     'abaixo', 'direita', 'direita'], dfs('12345_786'))
     self.assertEqual(['abaixo'], bfs('12345_786'))
     self.assertEqual(['abaixo'], astar_hamming('12345_786'))
+    self.assertEqual(['abaixo'], astar_manhattan('12345_786'))
     
   
 #pai = Nodo('2_3541687')
